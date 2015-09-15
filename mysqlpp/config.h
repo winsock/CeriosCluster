133e0c06aa1aff598decdc72b5f3712fe5c94bea
Andrew Querol <andrew@querol.me>
2015-09-15 13:51:25 -0500

    Add mysqlpp to my repo since the SVN import messed up the source.

---
+++ mysqlpp/config.h
@@ -0,0 +1,97 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define if you have __gnu_cxx:slist container in <ext/slist> */
+#define HAVE_EXT_SLIST 1
+
+/* Define if you have ::slist container in <slist> */
+/* #undef HAVE_GLOBAL_SLIST */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if getopt() is available in libiberty.h */
+/* #undef HAVE_LIBIBERTY_GETOPT */
+
+/* Define to 1 if you have the `intl' library (-lintl). */
+/* #undef HAVE_LIBINTL */
+
+/* Define to 1 if you have the `z' library (-lz). */
+/* #undef HAVE_LIBZ */
+
+/* Define if you have the localtime_r() facility */
+#define HAVE_LOCALTIME_R 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if your MySQL library has SSL functions */
+#define HAVE_MYSQL_SSL_SET /**/
+
+/* Define if getopt() is available in unistd.h */
+#define HAVE_POSIX_GETOPT /**/
+
+/* Define if you have POSIX threads libraries and header files. */
+/* #undef HAVE_PTHREAD */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have std::slist container in <slist> */
+/* #undef HAVE_STD_SLIST */
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <synch.h> header file. */
+/* #undef HAVE_SYNCH_H */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <zlib.h> header file. */
+/* #undef HAVE_ZLIB_H */
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "plusplus@lists.mysql.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "mysql++"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "mysql++ 3.2.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "mysql++"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.2.2"
+
+/* Define to necessary symbol if this constant uses a non-standard name on
+   your system. */
+/* #undef PTHREAD_CREATE_JOINABLE */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
